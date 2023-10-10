fea8cf94206bb7e90ff9759f6efa518a3e9cdc1b
La-mar-s <sony.lamar.1995@gmail.com>
2023-10-11 01:46:27 +0200

    done

--- DWA_16/src/App.jsx
+++ DWA_16/src/App.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
   return (
     <div>
       <Navbar />
-      <Card
-        img="katie-zaferes.png"
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <Hero />
+      <section className="cards--list">{cards}</section>
     </div>
   );
 }
+
+//props eg
+//       img={item.coverImg}
+//       rating={item.stats.rating}
+//       reviewCount={item.stats.reviewCount}
+//       location={item.location}
+//       title={item.title}
+//       price={item.price}
+//       openSpots={item.openSpots}
